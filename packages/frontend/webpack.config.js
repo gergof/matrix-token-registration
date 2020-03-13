6e12e37894cd1914fac1be9d6c181744fc1d1e19
Fándly Gergő <gergo@systemtest.tk>
2020-03-13 21:37:27 +0200

    ci: Init frontend

---
+++ packages/frontend/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	mode: process.env.NODE_ENV || 'development',
+	devtool: 'source-map',
+	entry: 'src/index.js',
+	output: {
+		path: path.join(__dirname, 'build'),
+		filename: 'index.js'
+	},
+	module: {
+		rule: [
+			{
+				test: /\.jsx?$/,
+				exclue: /node_modules/,
+				use: 'babel-loader'
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.js', '.jsx']
+	},
+	plugins: [
+		new HtmlPlugin({
+			hash: true,
+			filename: 'index.html',
+			template: 'src/public/index.html'
+		})
+	],
+	devServer: {
+		historyApiFallback: true
+	}
+};
